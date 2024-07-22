02bbef1788ee5512ea70e596016dafe4f3b388f1
Aneesh Sharma <aneeshsharma1024@gmail.com>
2024-07-22 21:14:46 +0530

    Setup for Semaphore, Mutex Lock and Sockets

--- server.c
+++ server.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <time.h>
 #include <pthread.h>
+#include <semaphore.h>
+#include <sys/socket.h>
 
 #define MAX_CLIENTS 10
 
@@ -21,8 +23,39 @@ cache_element *find(char *url);                         // to find a cached resu
 int add_cache_element(char *data, int size, char *url); // to add a result to cache
 void remove_cache_element();                            // to remove the longest stored cache
 
-int port = 8080; // port for our socket
+int port_number = 8080; // port for our socket
 int proxy_socket_id;
 pthread_t pid[MAX_CLIENTS];
-// sem_t semaphore;
-pthread_mutex_t lock;
+sem_t semaphore;      // used for process synchronization
+pthread_mutex_t lock; // same as semaphore only two values - on and off
+
+cache_element *head;
+int cache_size;
+
+int main(int argc, char *const argv[])
+{
+    int client_socket_id, client_len;
+
+    /* Definition for sockaddr, i.e., a descripter for a network address
+        struct sockaddr{
+            unsigned short sa_family; // Address family (AF_INET - IPv4, AF_INET6 - IPv6, AF_UNIX - local address)
+            char sa_data[14]; // Address data
+        }
+    */
+    struct sockaddr server_addr, client_addr;
+    sem_init(&semaphore, 0, MAX_CLIENTS); // initializing a semaphore
+
+    pthread_mutex_init(&lock, NULL); // initializing a mutex lock
+
+    if(argc == 2){
+        port_number = atoi(argv[1]);
+    }else{
+        printf("Too few arguements\n");
+        exit(1);
+    }
+
+    printf("Starting proxy server at port: %d",port_number);
+
+    
+
+}
